573c45efe3586b28f60152dc418d3b3e1192880c
sljrobin <sljrobin@gmail.com>
2015-05-14 21:29:52 +0200

    Updated creator

--- creator.py
+++ creator.py
@@ -5,30 +5,8 @@
 # Description    : 
 # Author         :Simon L. J. Robin
 # Created        :2014-12-11 09:57:31
-# Modified       :2015-04-08 19:55:28
+# Modified       :2015-05-14 21:29:50
 ##########################################################################################
 
 #“A goal without a plan is just a wish.”
 # ― Antoine de Saint-Exupéry
-#
-import argparse
-import os
-
-#class Cleaner:
-##########################################################################################
-class Report:
-    "Creation of a report"
-    
-    def __init__(self):
-        self.name = "report.tex"
-        self.author = ""
-        self.title = ""
-        self.subtitle = ""
-
-
-##########################################################################################
-parser = argparse.ArgumentParser()
-
-parser.add_argument("-n", "--new", help="create a new report", action="store_true")
-args = parser.parse_args()
-
