c65feb0c3f2ef67a4ccedb1ab5e77f48f203c6b0
sljrobin <sljrobin@gmail.com>
2015-05-26 23:08:07 +0200

    Updated reportmaker

--- creator.py
+++ creator.py
@@ -5,8 +5,10 @@
 # Description    : 
 # Author         :Simon L. J. Robin
 # Created        :2014-12-11 09:57:31
-# Modified       :2015-05-14 21:29:50
+# Modified       :2015-05-26 23:08:03
 ##########################################################################################
 
 #“A goal without a plan is just a wish.”
 # ― Antoine de Saint-Exupéry
+
+# Constants
